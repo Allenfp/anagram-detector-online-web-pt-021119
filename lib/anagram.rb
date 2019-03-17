8208032636761002b6ed02fec0ee066935ff1a96
Allen F Partlow <allenfp@gmail.com>
2019-03-17 01:04:36 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,10 +10,10 @@ class Anagram
   def match(work_list)
     return_value = []
     work_list.each { |x|
-      puts x
+      x.split.sort
+      @word.split.sort
       if x.split.sort == @word.split.sort
         return_value.push(x)
-
       end
       }
     return_value
