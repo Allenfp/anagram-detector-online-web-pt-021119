8640d32bf22a15304477f29157e1e258ba7f5bb0
Allen F Partlow <allenfp@gmail.com>
2019-03-17 01:00:30 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,9 +4,15 @@ class Anagram
   attr_accessor :word
 
   def initialize(word)
-
     @word = word
+  end
 
+  def self.match(work_list)
+    work_list.each { |x|
+      if x.split.sort = @word.split.sort
+        return_value = x
+
+      }
   end
 
 end
